282dc809aee9058900e5a26cdc1c208144c81ed1
camille <106037608+rirum@users.noreply.github.com>
2023-07-12 16:27:46 -0300

    feat: prisma and user modules

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UsersRepository } from './user.repository';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, UsersRepository],
 })
 export class UserModule {}
